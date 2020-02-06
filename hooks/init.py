49268cec6d07f98ac566b80af91a40780a62a65c
Philip Scadding <pscadding@users.noreply.github.com>
2020-02-06 15:11:06 +0000

    Black (#7)
    * Added the Hound badge to the README.
    
    * Ran python-modernize
    
    * Ran the pre-commit hook
    
    * Added the black badge to the README.
    
    * Added the Azure config.

--- hooks/init.py
+++ hooks/init.py
@@ -1,26 +1,27 @@
 # Copyright (c) 2015 Shotgun Software Inc.
-# 
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 import sgtk
 import os
 
 HookBaseClass = sgtk.get_hook_baseclass()
 
+
 class ScreeningroomInit(HookBaseClass):
     """
     Controls the initialization in and around screening room
     """
-        
+
     def before_rv_launch(self, path):
         """
-        Executed before RV is being launched        
-        
+        Executed before RV is being launched
+
         :param path: The path to the RV that is about to be launched
         :type path: str
         """
@@ -30,6 +31,6 @@ class ScreeningroomInit(HookBaseClass):
         #
         # > app = self.parent
         # > current_entity = app.context.entity
-        
+
         # you can set environment variables like this:
         # os.environ["MY_SETTING"] = "foo bar"
